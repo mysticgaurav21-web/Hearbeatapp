cc760a657598e04e82669391cce784e5b1208a99
mysticgaurav21-web <mysticgaurav21@gmail.com>
2026-07-15 14:04:26 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base './' = relative paths — repo ka naam kuch bhi ho, GitHub Pages pe chalega
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
